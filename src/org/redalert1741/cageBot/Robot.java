aa6165103be9a51632f1bd6265e10f6e94cf5443
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2017-10-12 17:45:25 -0700

    add deadband function

--- src/org/redalert1741/cageBot/Robot.java
+++ src/org/redalert1741/cageBot/Robot.java
@@ -1,13 +1,18 @@
 package org.redalert1741.cageBot;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends IterativeRobot
 {
+	public XboxController driver, operator;
+	
 	@Override
 	public void robotInit()
 	{
-
+		driver = new XboxController(0);
+		operator = new XboxController(1);
 	}
 
 	@Override
@@ -25,7 +30,11 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopPeriodic()
 	{
-
+		double x = driver.getX(Hand.kRight);
+		double y = driver.getY(Hand.kLeft);
+		// do drive
+		
+		
 	}
 
 	@Override
@@ -33,5 +42,25 @@ public class Robot extends IterativeRobot
 	{
 
 	}
-}
+	
+	public double deadband(double x)
+	{
+		if (x >= 0.05) 
+		{
+			return 1/0.95 * x - 0.05;
+		}
+		else if (0.05 > Math.abs(x))
+		{
+			return 0;
+		}
+		else if (x <= -0.05)
+		{
+			return -1/0.95 * x + 0.05;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+ }
 
